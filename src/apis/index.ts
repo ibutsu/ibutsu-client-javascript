d5d0f4f319812bdcd711a015f73033786a0aa2f9
mshriver <mshriver@redhat.com>
2025-10-31 17:25:34 +0100

    Generate TS client with openapi gen 7

---
+++ src/apis/index.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AdminProjectManagementApi';
+export * from './AdminUserManagementApi';
+export * from './ArtifactApi';
+export * from './DashboardApi';
+export * from './GroupApi';
+export * from './HealthApi';
+export * from './ImportApi';
+export * from './LoginApi';
+export * from './ProjectApi';
+export * from './ResultApi';
+export * from './RunApi';
+export * from './TaskApi';
+export * from './UserApi';
+export * from './WidgetApi';
+export * from './WidgetConfigApi';
